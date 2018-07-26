0a6ab37d5c13ef6bc517c7b1bfc99192937515bb
williamorim <thewilliam89@gmail.com>
2018-07-26 10:35:35 -0300

    dados datasus sim

---
+++ scripts/web-scraping/web-scraping-sim.R
@@ -0,0 +1,27 @@
+# Web scraping do sistema de informações sobre mortalidade (SIM/DATASUS)
+
+library(magrittr)
+
+get_sim_data <- function(base, uf, ano) {
+  
+  url <- paste0(
+    "ftp://ftp.datasus.gov.br/dissemin/publicos/SIM/CID10/DORES/",
+    base,
+    uf,
+    ano, 
+    ".dbc"
+  )
+  
+  #download.file(url, destfile = "data/datasus-sim/temp.dbc")
+  
+  read.dbc::read.dbc(file = "data/datasus-sim/temp.dbc") %>% 
+    readr::write_rds(
+      path = paste0("data/datasus-sim/", base, uf, ano, ".rds"), 
+      compress = "gz"
+    )
+    
+}
+
+walk(2007:2016, get_sim_data, base = "DO", uf = "SP")
+
+
