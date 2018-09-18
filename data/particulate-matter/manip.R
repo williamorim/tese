d41390aad020eb650ad1688dc1c7ecf3e8c6f147
William <thewilliam89@gmail.com>
2018-09-18 10:35:55 -0300

    pm dataframe with valid data information

--- data/particulate-matter/manip.R
+++ data/particulate-matter/manip.R
@@ -16,12 +16,7 @@ df <- purrr::map_dfr(
 )
 
 df %>%
-  mutate(
-    mass_conc = ifelse(mass_conc == 999, NA, mass_conc),
-    hour_1 = ifelse(nchar(hour) == 1, paste0("0", hour, ":00:00"), paste0(hour, ":00:00")),
-    datetime = lubridate::ymd_hms(paste(date, hour_1)) - 1
-  ) %>%
-  select(-mass_conc_movel, -time, -hour_1) %>%
+  select(-mass_conc_movel) %>%
   write_rds(
     path = "data/particulate-matter/pm.rds",
     compress = "gz"
