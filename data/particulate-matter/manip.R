a964158d94ac89f733d5ea687d4f15232e6842b1
William <thewilliam89@gmail.com>
2018-09-17 22:59:21 -0300

    baixando dados de pm novamente

--- data/particulate-matter/manip.R
+++ data/particulate-matter/manip.R
@@ -2,28 +2,30 @@
 
 library(tidyverse)
 
-files <- list.files(path = "data/particulate-matter/")
-paths <- str_c("data/particulate-matter/", files)
-paths <- paths[str_detect(paths, ".csv")]
-paths <- paths[file.info(paths)$size > 1700]
+files <- list.files(
+  path = "data/particulate-matter/", 
+  pattern = ".csv",
+  full.names = TRUE
+)
 
-df <- map_dfr(
-  paths, 
+files <- files[file.info(files)$size > 1700]
+
+df <- purrr::map_dfr(
+  files, 
   read_csv
 )
 
-df %>% 
-  select(-time, -mass_conc_movel) %>% 
+df %>%
+  mutate(mass_conc = ifelse(mass_conc == 999, NA, mass_conc)) %>% 
+  rename(datetime = time) %>% 
+  select(-mass_conc_movel) %>% 
   write_rds(
     path = "data/particulate-matter/pm.rds",
     compress = "gz"
   )
 
+
 # Unidades de medida
 
-# 1 MP10 (Partículas Inaláveis)        µg/m3            
-# 2 NO2 (Dióxido de Nitrogênio)        µg/m3            
-# 3 CO (Monóxido de Carbono)           ppm              
-# 4 NO (Monóxido de Nitrogênio)        µg/m3            
-# 5 MP2.5 (Partículas Inaláveis Finas) µg/m3            
-# 6 O3 (Ozônio)                        µg/m3  
+# 1 MP10 (Partículas Inaláveis)        µg/m3          
+# 2 MP2.5 (Partículas Inaláveis Finas) µg/m3
