2fc5971a21e478a694c5fd1cd78ff48f0ea86cab
williamorim <thewilliam89@gmail.com>
2018-11-25 12:41:20 -0200

    texto e regressao segmentada

--- scripts/salvo-2017/salvo-reg-segmentada.R
+++ scripts/salvo-2017/salvo-reg-segmentada.R
@@ -34,6 +34,81 @@ formula <- df_model %>%
 
 # Model -------------------------------------------------------------------
 
+ajustar_modelo <- function(formula, df, pontos) {
+  model <- lm(formula, data = df)
+  segmented(model, seg.Z = ~share_gas, psi = pontos)
+}
+
+ajustar_modelo_segura <- purrr::possibly(ajustar_modelo, NA)
+
+calcular_erro <- function(modelo, df) {
+  
+  df %>% 
+    mutate(
+      pred = predict(modelo, df)
+    ) %>% 
+    filter(!is.na(o3_mass_conc), !is.na(pred)) %>% 
+    summarise(
+      sqr = sum((o3_mass_conc - pred)^2),
+      sqt = sum((o3_mass_conc - mean(o3_mass_conc))^2),
+      rmse = sqrt(sqr/length(pred)),
+      r2 = 1 - sqr/sqt
+    ) %>% 
+    select(rmse, r2)
+  
+}
+
+validacao_cruzada <- function(pontos, formula, df, k = 5) {
+  
+  df$fold <- sample.int(n = k, size = nrow(df), replace = TRUE)
+  
+  erros <- NULL
+  
+  for(i in 1:k) {
+    
+    df_treino <- filter(df, fold != i)
+    df_teste <- filter(df, fold == i)
+    
+    modelo <- ajustar_modelo_segura(formula, df_treino, pontos)
+    
+    if(!is.na(modelo)) {
+      erros <- bind_rows(
+        erros,
+        calcular_erro(modelo, df_teste)
+      )
+    }
+    
+  }
+  
+  print(
+    paste("Modelo para os pontos", paste(pontos, collapse = "-"), "finalizado!")
+  )
+  
+  erros %>% 
+    summarise(
+      rmse = mean(rmse),
+      r2 = mean(r2)
+    )
+  
+}
+
+pontos <- expand.grid(
+  p1 = seq(0.17, 0.23, 0.01),
+  p2 = seq(0.47, 0.53, 0.01),
+  p3 = seq(0.57, 0.63, 0.01)
+) %>% 
+  split(x = ., seq(nrow(.))) %>% 
+  map(as.numeric)
+
+set.seed(5893524)
+
+resultados <- map(
+  pontos,
+  validacao_cruzada,
+  formula = formula,
+  df = df_model
+)
+
 model <- lm(formula, data = df_model)
 seg_model <- segmented(model, seg.Z = ~share_gas, psi = c(0.2, 0.5, 0.6))
 
